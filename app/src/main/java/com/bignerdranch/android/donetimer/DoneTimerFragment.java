7fbe532ba240170425a08c8c58ee75201a4ca9c2
unknown <cookab1@CS-317-9K7KFQ1.appd.appstate.edu>
2017-11-28 11:14:31 -0500

    Added some needed Views

---
+++ app/src/main/java/com/bignerdranch/android/donetimer/DoneTimerFragment.java
@@ -0,0 +1,10 @@
+package com.bignerdranch.android.donetimer;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by cookab1 on 11/21/2017.
+ */
+
+public class DoneTimerFragment extends Fragment {
+}
