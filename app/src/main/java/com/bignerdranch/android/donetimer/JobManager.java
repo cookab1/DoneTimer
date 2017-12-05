37509d3b10bd3a86ac55bd5f68a485b04efca190
unknown <cookab1@CS-317-9K7KFQ1.appd.appstate.edu>
2017-12-05 10:55:10 -0500

    JobManager implemented

--- app/src/main/java/com/bignerdranch/android/donetimer/JobManager.java
+++ app/src/main/java/com/bignerdranch/android/donetimer/JobManager.java
@@ -1,8 +1,12 @@
 package com.bignerdranch.android.donetimer;
 
+import android.content.ContentValues;
 import android.content.Context;
 
+import java.io.File;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
 /**
  * Created by cookab1 on 12/5/2017.
@@ -10,7 +14,7 @@ import java.util.ArrayList;
 
 public class JobManager {
     private static JobManager sJobManager;
-    private ArrayList<Job> jobs;
+    private List<Job> jobs;
 
     public static JobManager get(Context context) {
         if (sJobManager == null) {
@@ -19,6 +23,48 @@ public class JobManager {
         return sJobManager;
     }
     private JobManager () {
-        jobs = new ArrayList<>();
+        jobs = new ArrayList<Job>();
+    }
+
+    public void addJob(Job c) {
+        jobs.add(c);
+    }
+
+    public List<Job> getJobs() {
+        return jobs;
+    }
+
+    //returns the job that is being looked for by the name
+    public Job getJob(String name) {
+        Job job = null;
+        for(int i = 0; i < jobs.size(); i++) {
+            if(jobs.get(i).getName().equals(name)) {
+                job = jobs.get(i);
+            }
+        }
+        if(job == null) {
+            System.out.println("Job not found in the jobs ArrayList.\nPlease ender a valid job name.");
+        }
+        return job;
+    }
+
+    //This function updates the specified job (name) in the jobs ArrayList.
+    public void updateJob(String name, Job newJob) {
+        int index = getIndex(name);
+
+        jobs.set(index, newJob);
+    }
+
+    private int getIndex(String name) {
+        int index = -1;
+        for(int i = 0; i < jobs.size(); i++) {
+            if(jobs.get(i).getName().equals(name)) {
+                index = i;
+            }
+        }
+        if(index < 0) {
+            System.out.println("Job not found in the jobs ArrayList.\nPlease ender a valid job name.");
+        }
+        return index;
     }
 }
